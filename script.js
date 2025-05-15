5596187a05c138700109df1156dbc5debf1312c4
Guilherme Kobylansky <06664631075@senacrs.edu.br>
2025-05-15 10:22:32 -0300

    finalização

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const url = `https://api.thecatapi.com/v1/breeds`;
+let url = `https://api.thecatapi.com/v1/breeds`;
 const api_key =`live_7w6FfosCkolmKR6I6j0O4jUZzwUZAlP17SoHlokWTzl605CTOLTCvSyApyIXDW7W`;
 const slcBreed = document.getElementById('slcBreed');
-const slcGen =document.getElementById('slcGen');
+
 
 let breedImg = document.getElementById('breedImg');
 let breeds = [];
+
+
 
 fetch(url,{headers: {
     'x-api-key': api_key
@@ -12,23 +14,26 @@ fetch(url,{headers: {
 .then(res => res.json())
 .then(data => {
     data = data.filter(img=> img.image?.url!=null)
+   
     breeds = data;
-    
-    slcBreed.innerHTML = `<option value="">Selecione uma raça</option>`;
-    breeds.forEach(breed => {
-        slcBreed.innerHTML += `<option value="${breed.id}">${breed.name}</option>`;
-        if(slcBreed.value == ""){
-            breedImg.alt = 'Selecione uma raça primeiro';
-            if(slcGen.value == ""){
-                breedImg.alt = 'Selecione uma quantidade primeiro';
-            }else{
-                let breedUrl = breed.image.url;
-                breedImg.src = breedUrl;
-            }
-        }
-    });
-});
 
+     for (let i = 0; i < breeds.length; i++) {
+      const breed = breeds[i];
+      let option = document.createElement('option');
+       
+       if(!breed.image)continue
+       
+      option.value = i;
+      option.innerHTML = `${breed.name}`;
+        slcBreed.appendChild(option);
+      };
+      
+  });
+  function showBreedImage(index){
+ 
+  breedImg.src= breeds[index].image.url;
+
+  }
 
 
 
